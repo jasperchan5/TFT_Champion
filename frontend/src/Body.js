0108d18bebebd871335a068bda653e8c13fd228f
jasperchan5 <jscnn51011@gmail.com>
2022-08-20 18:43:14 +0800

    Commit

--- frontend/src/Body.js
+++ frontend/src/Body.js
@@ -1,7 +1,7 @@
 import instance from "./axios"
 import { useEffect, useState } from 'react'
 import $ from 'jquery';
-import { Box, Tab } from '@mui/material'
+import { Box, Tab, Collapse } from '@mui/material'
 import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import { useNavigate } from 'react-router-dom';
@@ -11,7 +11,7 @@ import useQuery from './useQuery';
 export default () => {
     const navigate = useNavigate();
     let currSetIndex = '0';
-    let displayMode = "all";
+    const [displayMode,setDisplayMode] = useState(localStorage.getItem("displayMode") || "all");
     const set = [
         {text: 'Set 1', value: '0'},
         {text: 'Set 2', value: '1'},
@@ -28,18 +28,55 @@ export default () => {
     const queryStr = useQuery();
     try{
         currSetIndex = queryStr.setIndex.toString();
-        displayMode = queryStr.displayMode;
     }
     catch(e){}
+
+    const [TFTData,setTFTData] = useState([]);
+    const [champions,setChampions] = useState({});
+    const [championNames,setChampionNames] = useState([]);
+    useEffect(() => {
+        const fetchTFTData = async() => {
+            await instance.get('/api/getData').then((response) => {
+                setChampionNames(Object.keys(response.data));
+                setTFTData(response.data);
+            })
+            $.getJSON("http://ddragon.leagueoflegends.com/cdn/12.15.1/data/en_US/champion.json", (result) => {
+                console.log(result.data);
+                const resultKeys = Object.keys(result.data);
+                const lowerCaseKeys = resultKeys.map((e) => e.toLowerCase());
+                resultKeys.forEach((e,i) => {
+                    result.data[lowerCaseKeys[i]] = result.data[e];
+                    delete result.data[e];
+                })
+                result.data["wukong"] = result.data["monkeyking"];
+                delete result.data["monkeyking"];
+                result.data["nunu & willump"] = result.data["nunu"];
+                delete result.data["nunu"];
+                setChampions(result.data);
+            })
+        }
+        fetchTFTData();
+    },[])
+
+    useEffect(() => {
+        if(displayMode === 'all'){
+            document.getElementsByClassName("all")[0].classList.add("selected");
+            document.getElementsByClassName("bySet")[0].classList.remove("selected");
+        }
+        else if(displayMode === 'bySet'){
+            document.getElementsByClassName("bySet")[0].classList.add("selected");
+            document.getElementsByClassName("all")[0].classList.remove("selected");
+        }
+    })
 
     const SetTabs = () => {
         const [value,setvalue] = useState(currSetIndex);
         const handleChange = (_,e) => {
             setvalue(e);
-            navigate({search: `?displayMode=bySet&setIndex=${e}`})
+            navigate({search: `?setIndex=${e}`})
         };
         return (
-            <Box sx={{ width: '80%', typography: 'body1', margin: "auto", display: "flex", alignItems: "center" }}>
+            <Box sx={{ width: '80%', typography: 'body1', margin: "24px auto 12px auto", display: "flex", alignItems: "center" }}>
                 <TabContext value={value}>
                     <Box sx={{ width: "100%", borderColor: 'divider' }}>
                         <TabList centered={true} TabIndicatorProps={{style: {background: "#202325", height: "2px"}}} onChange={handleChange} aria-label="lab API tabs example">     
@@ -52,80 +89,95 @@ export default () => {
             </Box>
         )
     }
-    const [TFTData,setTFTData] = useState([]);
-    const [champions,setChampions] = useState([]);
-    const [championNames,setChampionNames] = useState([]);
-    useEffect(() => {
-        const fetchTFTData = async() => {
-            await instance.get('/api/getData').then((response) => {
-                setChampionNames(Object.keys(response.data));
-                setTFTData(response.data);
-            })
-            $.getJSON("http://ddragon.leagueoflegends.com/cdn/12.15.1/data/en_US/champion.json", (result) => {
-                setChampions(result.data);
-            })
-        }
-        fetchTFTData();
-    },[])
     const ChampionRender = () => {
         const ChampionCell = ({name}) => {
             let processedName = name;
             if(processedName.match(/\w+[']{1}\w+/)){
                 processedName = processedName.split("'").join().replace(",","");
             }
-            else if(processedName.match(/\w[ ]{1}\w/)){
+            else if(processedName.match(/\w[ ]{1}\w/) && processedName !== 'Renata Glasc'){
                 processedName = processedName.split(" ").join().replace(",","");
             }
-            const currentSet = set[parseInt(currSetIndex)].text.replace(" ","_").toLowerCase();
-            if(displayMode === 'bySet'){
-                const exist = Object.keys(TFTData[name]).indexOf(currentSet) !== -1;
-                let skin = "";
-                if(TFTData[name][currentSet] !== undefined){
-                    skin = TFTData[name][currentSet].skin || "";
-                }
-                return (
-                    <div className="champion_cell" style={{display: exist ? "flex" : "none"}}>
-                        <img src={`http://ddragon.leagueoflegends.com/cdn/12.15.1/img/champion/${processedName}.png`}/>
-                        <div>{name}</div>
-                        <div>{skin}</div>
-                    </div>
-                )
+            else if(processedName.match(/\w[.]{1}[ ]{1}\w/)){
+                processedName = processedName.replace(" ","").split(".").join().replace(",","");
             }
-            else if(displayMode === 'all'){
-                return (
-                    <div className="champion_cell">
-                        <img src={`http://ddragon.leagueoflegends.com/cdn/12.15.1/img/champion/${processedName}.png`}/>
-                        <div>{name}</div>
-                    </div>
-                )
+            const currentSet = set[parseInt(currSetIndex)].text.replace(" ","_").toLowerCase();
+            processedName = processedName.toLowerCase();
+            if(processedName === 'renata glasc'){
+                processedName = 'renata';
+            }
+            const exist = Object.keys(TFTData[name]).indexOf(currentSet) !== -1;
+            try{
+                if(displayMode === 'bySet'){
+                    let skin = "";
+                    let origin = [];
+                    let classes = [];
+                    if(TFTData[name][currentSet] !== undefined){
+                        skin = TFTData[name][currentSet].skin;
+                        origin = TFTData[name][currentSet].origin;
+                        classes = TFTData[name][currentSet]["class"];
+                    }
+                    return (
+                        <div className="champion_cell" style={{display: exist ? "flex" : "none"}}>
+                            <img width={96} height={96} src={`http://ddragon.leagueoflegends.com/cdn/12.15.1/img/champion/${champions[processedName].image.full}`}/>
+                            <div>{name}</div>
+                            <div style={{fontSize: "14px", color: "#AD1546"}}>{skin}</div>
+                            {origin.map((e) => <div style={{fontSize: "14px", color: "#72777A"}}>{e}</div>)}
+                            {classes.map((e) => <div style={{fontSize: "14px", color: "#72777A"}}>{e}</div>)}
+                        </div>
+                    )
+                }
+                else if(displayMode === 'all'){
+                    return (
+                        <div className="champion_cell">
+                            <img width={96} height={96} src={`http://ddragon.leagueoflegends.com/cdn/12.15.1/img/champion/${champions[processedName].image.full}`}/>
+                            <div>{name}</div>
+                        </div>
+                    )
+                }
+            }
+            catch(e){
+                if(exist){
+                    let skin = "";
+                    let origin = [];
+                    let classes = [];
+                    if(TFTData[name][currentSet] !== undefined){
+                        skin = TFTData[name][currentSet].skin;
+                        origin = TFTData[name][currentSet].origin;
+                        classes = TFTData[name][currentSet]["class"];
+                    }
+                    return (
+                        <div className="champion_cell">
+                            <img width={96} height={96} src={`http://ddragon.leagueoflegends.com/cdn/12.15.1/img/spell/SummonerMana.png`}/>
+                            <div>{name}</div>
+                            {origin.map((e) => <div style={{fontSize: "14px", color: "#72777A"}}>{e}</div>)}
+                            {classes.map((e) => <div style={{fontSize: "14px", color: "#72777A"}}>{e}</div>)}
+                        </div>
+                    )
+                }
             }
         }
         return (
             <>
-                <Box sx={{ width: "80%", margin: "24px auto 24px auto", display: "grid", rowGap: "12px", gridTemplateColumns: "repeat(8,1fr)"}}>
+                <Box sx={{ width: "80%", margin: "24px auto 24px auto", display: "grid", rowGap: "12px", gridTemplateColumns: "repeat(6,1fr)"}}>
                     {championNames.map((e) => <ChampionCell name={e}/>)}
                 </Box>
             </>
         )
     }
-    const TabRender = () => {
-        switch (displayMode) {
-            case 'all':
-                return <div className="all" onClick={() => navigate({search: `?displayMode=all`})}>All</div>
-            case 'bySet':
-                return <>
-                    <div className="all" onClick={() => navigate({search: `?displayMode=all`})}>All</div>
-                    <SetTabs/>
-                </>
-            default:
-                break;
-        }
-    }
     return (
         <div id="Body">
-            
-            <div className="all" onClick={() => navigate({search: `?displayMode=bySet&setIndex=${0}`})}>By Set</div>
-            <TabRender/>
+            <Box sx={{width: "80%", height: "48px", display: "flex", margin: "auto", justifyContent: "space-around", alignItems: "center"}}>
+                <div className="all" onClick={() => {
+                    setDisplayMode("all");
+                    localStorage.setItem("displayMode","all");
+                }}>All</div>
+                <div className="bySet" onClick={() => {
+                    setDisplayMode("bySet");
+                    localStorage.setItem("displayMode","bySet");
+                }}>By Set</div>
+            </Box>
+            {displayMode === 'bySet' ? <SetTabs/> : <></>}
             <ChampionRender/>
         </div>
     )
